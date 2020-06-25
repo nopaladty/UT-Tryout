79852faa511859fe48cb1601071614ac6c656c50
Naufal Aditya Putra <56852872+nopaladty@users.noreply.github.com>
2020-06-25 13:46:28 +0700

    Create 003.js

---
+++ 003.js
@@ -0,0 +1,8 @@
+var array = [98, 85, 75];
+var largest = Math.max.apply(Math, array);
+
+var array = [98, 85, 75];
+var largest = Math.min.apply(Math, array);
+
+var array = [98, 85, 75];
+var largest = Math.avg.apply(Math, array);
