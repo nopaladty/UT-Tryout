09795b01c80d96942aff992d502f4a65a1963938
Naufal Aditya Putra <56852872+nopaladty@users.noreply.github.com>
2020-06-25 20:43:57 +0700

    Create 0010.js

---
+++ 0010.js
@@ -0,0 +1,6 @@
+var json_data = '{"user" : "agusph", "display_name" : "Agus Prawoto Hadi", "website" : "Jagowebdev.com"}';
+obj = JSON.parse(json_data);
+
+// Objek hanya bisa di buka melalui console
+console.log(obj);
+console.log(obj.display_name);
