2154717cc9fc4c818375fdd678d31e6fe8e927f5
Naufal Aditya Putra <56852872+nopaladty@users.noreply.github.com>
2020-06-25 13:09:28 +0700

    Create 001.js

---
+++ 001.js
@@ -0,0 +1,15 @@
+if (avg > 90){
+          console.log("Grade : A");      
+          }
+else if (avg <= 80 avg <= 89) {
+          console.log("Grade : B"); 
+          } 
+else if (avg <= 70 avg <= 79) {
+          console.log("Grade : C"); 
+          } 
+else if (avg <= 60 avg <= 69) {
+          console.log("Grade : D"); 
+          } 
+ else if (avg < 59) {
+          console.log("Grade : E"); 
+            } 
